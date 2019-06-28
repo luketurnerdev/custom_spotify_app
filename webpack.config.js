e58c695bd06fc9dd10c09728fa529247faf8cc40
Luke Turner <luke@lukedturner.com>
2019-06-28 16:33:02 +1000

    Setup source map for webapck so we can debug properly with minified js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+// const productionConfig = merge([
 
+//   parts.generateSourceMaps({ type: "source-map" }),
+
+//   ...
+// ]);
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  devtool: 'source-map'
 };
