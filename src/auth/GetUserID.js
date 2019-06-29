1f8f71d42877fbb9755b1bf9c1215d885867a890
Luke Turner <luke@lukedturner.com>
2019-06-29 13:30:31 +1000

    fixed spotifyInit from previous github code

---
+++ src/auth/GetUserID.js
@@ -0,0 +1,18 @@
+const GetUserID = () => {
+    return $.ajax({
+        type: 'GET',
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+          //Returns the ID to be used by other functions
+          
+          userID = response.id;
+          console.log('Lukes user id:' + response.id);
+        }
+      });
+
+};
+
+export default GetUserID;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 //This is the main JS script. Other scripts will be imported here
 import SpotifyInit from "./SpotifyInit";
+import ViewTopTracks from "./ViewTopTracks";
+import ViewPlaylists from "./ViewPlaylists";
+
+SpotifyInit();
+
+
+
+
+
+//Setup button clicks
+
+// document.getElementById('obtain-new-token').addEventListener('click', obtainNewToken, false);
+// document.getElementById("view-top-tracks").addEventListener('click', viewTopTracks);
+
+// document.getElementById("generate-playlist").addEventListener('click', generatePlaylist);
+document.getElementById("view-user-playlists").addEventListener('click', ViewPlaylists);
+
+//Saving the users selections for playlist options
+// document.getElementById("time-selector").addEventListener('click', saveTimeData);
+// document.getElementById("amount-of-tracks").addEventListener('click', saveTrackAmount);
 
 
 /// RUN DEFAULT SPOTIFY CODE ///
-SpotifyInit();
+
+//Debug 
+// ViewPlaylists();
+
+
 
 
 console.log('Index.js file ran');
