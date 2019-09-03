f0b189b1fa67c4000a9c3757c79e0eda491f6518
Luke Turner <luke@lukedturner.com>
2019-09-03 13:25:31 +1000

    Successfully returned user info on button click

--- src/controllers/playlist_controller.js
+++ src/controllers/playlist_controller.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
-export function viewPlaylists(req, res) {
-    console.log('inside');
-    // userInfo = await axios.get("/auth/getuser")
-    // .then(resp => {
-    //     console.log(resp);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // })
+export async function viewPlaylists(req, res) {
+    const userInfo = await axios.get("/auth/getuser")
+    .then(resp => {
+        console.log(resp);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
-    // console.log(userInfo);
-    // // res.redirect("/")
+    console.log(userInfo);
+    // res.redirect("/")
   
 }
 // export default viewPlaylists;
