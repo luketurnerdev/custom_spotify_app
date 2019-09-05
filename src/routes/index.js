1619349fdb077163f8d2885e887591187d124af9
Luke Turner <luke@lukedturner.com>
2019-09-05 15:48:58 +1000

    Successful GET to /users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth_routes");
+const usersRoutes = require("./users_routes");
+
 
 const pagesController = require("./../controllers/pages_controller")
 
@@ -9,6 +11,8 @@ router.get("/", pagesController.homepage);
 // Auth routes
 router.use("/auth", authRoutes);
 
+// Users routes
+router.use("/users", usersRoutes);
 
 
 module.exports = router;
