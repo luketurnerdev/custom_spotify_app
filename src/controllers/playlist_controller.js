58ecd08365d2ad5a247b9bc4a5a5f47cfa892904
Luke Turner <luke@lukedturner.com>
2019-09-05 19:07:50 +1000

    Successfully retrieve access token and userid on playlist button press but now its not broken

--- src/controllers/playlist_controller.js
+++ src/controllers/playlist_controller.js
@@ -1,57 +1,65 @@
 const axios = require("axios");
-let accessToken;
-let userID;
-export async function viewPlaylists(req, res) {
-
+const usersController = require("./users_controller")
+export async function viewPlaylists(req, res, next) {
+    let accessToken = ""
+    let userID = ""
+    
     const userInfo = await axios.get("http://localhost:8888/users/me")
     .then(resp => {
-        console.log("####")
-        console.log(resp.data)
-        const accessToken = resp.data.access_token;
-        const userID = resp.data.spotify_uid;
-        console.log(userID);
+        console.log(resp)
+        accessToken = resp.data.access_token;
+        userID = resp.data.spotify_uid;
     })
     .catch(err => {
         console.log(err);
     })
-        // const config = { 
-        //     headers: {
-        //         "Content-Type": 'application/x-www-form-urlencoded',
-        //         "Authorization": `Bearer ${accessToken}`
-        //     }
-        // };
 
-        //     const response = await axios.get
-        // (
-        //     `https://api.spotify.com/v1/users/${userID}/playlists?limit=50`,
-        //     config
-        // )
+    console.log(accessToken)
+    console.log(userID)
+
+
+    Playlist API call
+     const config = { 
+            headers: {
+                "Content-Type": 'application/x-www-form-urlencoded',
+                "Authorization": `Bearer ${accessToken}`
+            }
+        };
+
+        console.log(userID)
+
+            const response = await axios.get
+        (
+            `https://api.spotify.com/v1/users/${userID}/playlists?limit=50`,
+            config
+        )
         
 
-        // let playlists = response.data.items;
-        // let currentInfo = document.createElement('div');
-        // currentInfo.id = "current-info";
+        let playlists = response.data.items;
+        console.log(playlists)
+        let currentInfo = document.createElement('div');
+        currentInfo.id = "current-info";
 
-        // for (let i=0; i<playlists.length; i++) {
+        for (let i=0; i<playlists.length; i++) {
         
-        //     //Create playlist link
-        //     let a = document.createElement('a');
-        //     let linkText = document.createTextNode(playlists[i].name)
-        //     a.appendChild(linkText);
-        //     a.href = playlists[i].external_urls.spotify;
+            //Create playlist link
+            let a = document.createElement('a');
+            let linkText = document.createTextNode(playlists[i].name)
+            a.appendChild(linkText);
+            a.href = playlists[i].external_urls.spotify;
     
-        //     //Add to list
-        //     let newPlaylist = document.createElement("li");
-        //     newPlaylist.appendChild(a);
-        //     document.getElementById('playlist-container').appendChild(newPlaylist);
+            //Add to list
+            let newPlaylist = document.createElement("li");
+            newPlaylist.appendChild(a);
+            document.getElementById('playlist-container').appendChild(newPlaylist);
     
-        //     //Add delete button
-        //     let deleteButton = document.createElement('button');
-        //     deleteButton.textContent = `Delete Playlist`;
-        //     deleteButton.id = playlists[i].id;
-        //     newPlaylist.appendChild(deleteButton);
-        //     // document.getElementById(deleteButton.id).addEventListener('click', deletePlaylist);
+            //Add delete button
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = `Delete Playlist`;
+            deleteButton.id = playlists[i].id;
+            newPlaylist.appendChild(deleteButton);
+            // document.getElementById(deleteButton.id).addEventListener('click', deletePlaylist);
         
-        //   }
+          }
   
 }
